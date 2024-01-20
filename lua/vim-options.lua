efe30040f2b70e7fb03bf45e6645a2fc9d5f7799
rjachuthan <riturajachuthan@gmail.com>
2024-01-20 09:24:14 +0530

    Added Commenting Functionality

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,7 +1,7 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+-- vim.cmd("set tabstop=2")
+-- vim.cmd("set softtabstop=2")
+-- vim.cmd("set shiftwidth=2")
 
 -- Show vertical ruler after 80 characters
 vim.opt.colorcolumn = "81"
