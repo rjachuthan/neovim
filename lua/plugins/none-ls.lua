a38604ce4890fb8b8d7ac476f605c3977e7ee7a9
rjachuthan <riturajachuthan@gmail.com>
2024-01-28 14:49:42 +0530

    Added formatter, linter, code checker for shell

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,20 +1,24 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.prettier,
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.shellharden,
 
-        null_ls.builtins.diagnostics.ruff,
-        null_ls.builtins.diagnostics.eslint,
-      },
-    })
+				null_ls.builtins.diagnostics.ruff,
+				null_ls.builtins.diagnostics.eslint,
+				null_ls.builtins.diagnostics.shellcheck,
 
-    vim.keymap.set("n", "<leader>fd", vim.lsp.buf.format, {})
-  end,
+				null_ls.builtins.code_actions.shellcheck,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>fd", vim.lsp.buf.format, {})
+	end,
 }
