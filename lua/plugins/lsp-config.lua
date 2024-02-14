efe1ef8e58a082825d3c4a3107cd8ee581ba4a15
rjachuthan <riturajachuthan@gmail.com>
2024-02-14 18:59:16 +0530

    Added basic languages for mason

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,7 +12,16 @@ return {
 		opts = { auto_install = true },
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "pyright" },
+				ensure_installed = {
+					"bashls",
+					"html",
+					"cssls",
+					"jsonls",
+					"marksman",
+					"lua_ls",
+					"tsserver",
+					"pyright",
+				},
 			})
 		end,
 	},
