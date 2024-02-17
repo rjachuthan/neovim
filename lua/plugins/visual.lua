3f5670b61eb9e50b6cacac14ac4e0edc43ffc417
rjachuthan <riturajachuthan@gmail.com>
2024-02-17 13:56:44 +0530

    Moved Noice to Visual file

--- lua/plugins/visual.lua
+++ lua/plugins/visual.lua
@@ -1,16 +1,50 @@
 return {
-	{
-		"lukas-reineke/virt-column.nvim",
-		config = function()
-			require("virt-column").setup({
-				-- char = "|",
-				virtcolumn = "80,120",
-			})
-		end,
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		opts = {},
-	},
+  {
+    "lukas-reineke/virt-column.nvim",
+    config = function()
+      require("virt-column").setup({
+        -- char = "|",
+        virtcolumn = "80,120",
+      })
+    end,
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {},
+    config = function()
+      require("ibl").setup()
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("notify").setup({
+        background_colour = "#000000",
+      })
+      require("noice").setup({
+        lsp = {
+          -- Override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        -- You can enable a preset for easier configuration
+        presets = {
+          bottom_search = true,    -- use a classic bottom cmdline for search
+          command_palette = true,  -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false,      -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false,  -- add a border to hover docs and signature help
+        },
+      })
+    end,
+  },
 }
