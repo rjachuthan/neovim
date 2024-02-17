677fd6265f987e75b90cae07d319a0d18bb6c18d
rjachuthan <riturajachuthan@gmail.com>
2024-02-17 14:01:31 +0530

    Move colorscheme to visual.lua

--- lua/plugins/visual.lua
+++ lua/plugins/visual.lua
@@ -1,4 +1,29 @@
 return {
+  -- { "catppuccin/nvim" },
+  {
+    "rose-pine/neovim",
+    lazy = false,
+    -- name = "catppuccin",
+    name = "rose-pine",
+    priority = 1000,
+    config = function()
+      vim.o.termguicolors = true
+      vim.o.background = "dark"
+      require("rose-pine").setup({
+        variant = "auto",
+        styles = {
+          bold = true,
+          italic = true,
+          transparency = true,
+        },
+      })
+      vim.cmd("silent! colorscheme rose-pine")
+
+      -- Uncomment these lines for Transparency
+      -- vim.cmd("hi Normal guibg=none")
+      -- vim.cmd("hi clear SignColumn")
+    end,
+  },
   {
     "lukas-reineke/virt-column.nvim",
     config = function()
