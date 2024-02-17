7e2ea84eae0f8d894429776231356b5179b1b00c
rjachuthan <riturajachuthan@gmail.com>
2024-02-17 13:54:01 +0530

    Added intend blankline

--- lua/plugins/visual.lua
+++ lua/plugins/visual.lua
@@ -1,9 +1,16 @@
 return {
-	"lukas-reineke/virt-column.nvim",
-	config = function()
-		require("virt-column").setup({
-                        -- char = "|",
-                        virtcolumn = "80,120"
-                })
-	end,
+	{
+		"lukas-reineke/virt-column.nvim",
+		config = function()
+			require("virt-column").setup({
+				-- char = "|",
+				virtcolumn = "80,120",
+			})
+		end,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {},
+	},
 }
