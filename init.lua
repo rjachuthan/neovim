5eac5449da26f2f46ed9d02c209cb59056b113a8
rjachuthan <riturajachuthan@gmail.com>
2024-02-24 17:09:26 +0530

    Intro message

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
+-- [[
+-- This is a Personalized NeoVim Configuration
+-- ]]
+
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
