4557b54e43d8fc648188b8590deaeb752d2abd45
rjachuthan <riturajachuthan@gmail.com>
2024-03-15 08:25:11 +0530

    Debugger initial commit

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,121 +1,122 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
-			{ "j-hui/fidget.nvim", opts = {} },
-		},
-		config = function()
-			-- LSP servers and clients are able to communicate to each other what features they support
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      { "j-hui/fidget.nvim", opts = {} },
+    },
+    config = function()
+      -- LSP servers and clients are able to communicate to each other what features they support
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-			-- Setting up list of LSP Servers to be installed
-			local servers = {
-				pyright = {}, -- pip install pyright
-				bashls = {}, -- npm i -g bash-language-server
-				html = {}, -- npm i -g vscode-langservers-extracted
-				emmet_ls = {}, -- npm install -g emmet-ls
-				marksman = {},
-				tailwindcss = {}, -- npm install -g @tailwindcss/language-server
-				jsonls = {}, -- npm i -g vscode-langservers-extracted
-				tsserver = {}, -- npm install -g typescript typescript-language-server
-				lua_ls = {
-					settings = {
-						Lua = {
-							diagnostics = { globals = { "vim" } },
-						},
-					},
-				},
-			}
-			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, { "stylua" })
+      -- Setting up list of LSP Servers to be installed
+      local servers = {
+        pyright = {}, -- pip install pyright
+        bashls = {},  -- npm i -g bash-language-server
+        html = {},    -- npm i -g vscode-langservers-extracted
+        emmet_ls = {}, -- npm install -g emmet-ls
+        marksman = {},
+        tailwindcss = {}, -- npm install -g @tailwindcss/language-server
+        jsonls = {},  -- npm i -g vscode-langservers-extracted
+        tsserver = {}, -- npm install -g typescript typescript-language-server
+        debugpy = {},
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = { globals = { "vim" } },
+            },
+          },
+        },
+      }
+      local ensure_installed = vim.tbl_keys(servers or {})
+      vim.list_extend(ensure_installed, { "stylua" })
 
-			-- Setup language servers.
-			require("mason").setup()
-			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-			require("mason-lspconfig").setup({
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						require("lspconfig")[server_name].setup({
-							cmd = server.cmd,
-							settings = server.settings,
-							filetypes = server.filetypes,
-							-- This handles overriding only values explicitly passed
-							-- by the server configuration above. Useful when disabling
-							-- certain features of an LSP (for example, turning off formatting for tsserver)
-							capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {}),
-						})
-					end,
-				},
-			})
+      -- Setup language servers.
+      require("mason").setup()
+      require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+      require("mason-lspconfig").setup({
+        handlers = {
+          function(server_name)
+            local server = servers[server_name] or {}
+            require("lspconfig")[server_name].setup({
+              cmd = server.cmd,
+              settings = server.settings,
+              filetypes = server.filetypes,
+              -- This handles overriding only values explicitly passed
+              -- by the server configuration above. Useful when disabling
+              -- certain features of an LSP (for example, turning off formatting for tsserver)
+              capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {}),
+            })
+          end,
+        },
+      })
 
-			-- Global mappings.
-			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
-			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+      -- Global mappings.
+      -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+      vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
-			-- Use LspAttach autocommand to only map the following keys
-			-- after the language server attaches to the current buffer
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				-- Enable completion triggered by <c-x><c-o>
-				callback = function(event)
-					vim.bo[event.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-					-- Creating a function to make easy keybindings
-					local map = function(keys, func, desc)
-						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-					end
+      -- Use LspAttach autocommand to only map the following keys
+      -- after the language server attaches to the current buffer
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        -- Enable completion triggered by <c-x><c-o>
+        callback = function(event)
+          vim.bo[event.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+          -- Creating a function to make easy keybindings
+          local map = function(keys, func, desc)
+            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+          end
 
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-					map(
-						"<leader>ws",
-						require("telescope.builtin").lsp_dynamic_workspace_symbols,
-						"[W]orkspace [S]ymbols"
-					)
-					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-					map("<leader>ca", function()
-						vim.lsp.buf.code_action({ context = { only = { "quickfix", "refactor", "source" } } })
-					end, "[C]ode [A]ction")
-					map("K", vim.lsp.buf.hover, "Hover Documentation")
-					map("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+          map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+          map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+          map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+          map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+          map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+          map(
+            "<leader>ws",
+            require("telescope.builtin").lsp_dynamic_workspace_symbols,
+            "[W]orkspace [S]ymbols"
+          )
+          map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+          map("<leader>ca", function()
+            vim.lsp.buf.code_action({ context = { only = { "quickfix", "refactor", "source" } } })
+          end, "[C]ode [A]ction")
+          map("K", vim.lsp.buf.hover, "Hover Documentation")
+          map("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+          map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-					-- The following two autocommands are used to highlight references of the
-					-- word under your cursor when your cursor rests there for a little while.
-					--    See `:help CursorHold` for information about when this is executed
-					if not vim.fn.match(vim.fn.expand('%:t'), '\\v\\.(html|md)$') then
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-							buffer = event.buf,
-							callback = vim.lsp.buf.document_highlight,
-						})
-					end
+          -- The following two autocommands are used to highlight references of the
+          -- word under your cursor when your cursor rests there for a little while.
+          --    See `:help CursorHold` for information about when this is executed
+          if not vim.fn.match(vim.fn.expand("%:t"), "\\v\\.(html|md)$") then
+            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+              buffer = event.buf,
+              callback = vim.lsp.buf.document_highlight,
+            })
+          end
 
-					-- When you move your cursor, the highlights will be cleared
-					if not vim.fn.match(vim.fn.expand('%:t'), '\\v\\.(html|md)$') then
-						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-							buffer = event.buf,
-							callback = vim.lsp.buf.clear_references,
-						})
-					end
-				end,
-			})
-		end,
-	},
-	-- {
-	--   "artemave/workspace-diagnostics.nvim",
-	--   lazy = true,
-	--   config = function()
-	--     require("lazy").setup({ "workspace-diagnostics.nvim" })
-	--   end,
-	-- },
+          -- When you move your cursor, the highlights will be cleared
+          if not vim.fn.match(vim.fn.expand("%:t"), "\\v\\.(html|md)$") then
+            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+              buffer = event.buf,
+              callback = vim.lsp.buf.clear_references,
+            })
+          end
+        end,
+      })
+    end,
+  },
+  -- {
+  --   "artemave/workspace-diagnostics.nvim",
+  --   lazy = true,
+  --   config = function()
+  --     require("lazy").setup({ "workspace-diagnostics.nvim" })
+  --   end,
+  -- },
 }
