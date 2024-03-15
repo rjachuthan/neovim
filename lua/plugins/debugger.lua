5c0f79d867ba0e84042521bc46b9c67ea606f202
rjachuthan <riturajachuthan@gmail.com>
2024-03-15 08:30:46 +0530

    Add dap virutal text

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -1,77 +1,82 @@
 return {
-  {
-    "mfussenegger/nvim-dap",
-    config = function()
-      vim.keymap.set("n", "<F5>", function()
-        require("dap").continue()
-      end)
-      vim.keymap.set("n", "<F10>", function()
-        require("dap").step_over()
-      end)
-      vim.keymap.set("n", "<F11>", function()
-        require("dap").step_into()
-      end)
-      vim.keymap.set("n", "<F12>", function()
-        require("dap").step_out()
-      end)
-      vim.keymap.set("n", "<Leader>b", function()
-        require("dap").toggle_breakpoint()
-      end)
-      vim.keymap.set("n", "<Leader>B", function()
-        require("dap").set_breakpoint()
-      end)
-      vim.keymap.set("n", "<Leader>lp", function()
-        require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-      end)
-      vim.keymap.set("n", "<Leader>dr", function()
-        require("dap").repl.open()
-      end)
-      vim.keymap.set("n", "<Leader>dl", function()
-        require("dap").run_last()
-      end)
-      vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
-        require("dap.ui.widgets").hover()
-      end)
-      vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
-        require("dap.ui.widgets").preview()
-      end)
-      vim.keymap.set("n", "<Leader>df", function()
-        local widgets = require("dap.ui.widgets")
-        widgets.centered_float(widgets.frames)
-      end)
-      vim.keymap.set("n", "<Leader>ds", function()
-        local widgets = require("dap.ui.widgets")
-        widgets.centered_float(widgets.scopes)
-      end)
-    end,
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap" },
-    config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-      dapui.setup()
+	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			vim.keymap.set("n", "<F5>", function()
+				require("dap").continue()
+			end)
+			vim.keymap.set("n", "<F10>", function()
+				require("dap").step_over()
+			end)
+			vim.keymap.set("n", "<F11>", function()
+				require("dap").step_into()
+			end)
+			vim.keymap.set("n", "<F12>", function()
+				require("dap").step_out()
+			end)
+			vim.keymap.set("n", "<Leader>b", function()
+				require("dap").toggle_breakpoint()
+			end)
+			vim.keymap.set("n", "<Leader>B", function()
+				require("dap").set_breakpoint()
+			end)
+			vim.keymap.set("n", "<Leader>lp", function()
+				require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+			end)
+			vim.keymap.set("n", "<Leader>dr", function()
+				require("dap").repl.open()
+			end)
+			vim.keymap.set("n", "<Leader>dl", function()
+				require("dap").run_last()
+			end)
+			vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
+				require("dap.ui.widgets").hover()
+			end)
+			vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
+				require("dap.ui.widgets").preview()
+			end)
+			vim.keymap.set("n", "<Leader>df", function()
+				local widgets = require("dap.ui.widgets")
+				widgets.centered_float(widgets.frames)
+			end)
+			vim.keymap.set("n", "<Leader>ds", function()
+				local widgets = require("dap.ui.widgets")
+				widgets.centered_float(widgets.scopes)
+			end)
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap" },
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+			dapui.setup()
 
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end,
-  },
-  {
-    "mfussenegger/nvim-dap-python",
-    ft = "python",
-    dependencies = { "mfussenegger/nvim-dap", "rcarriga/nvim-dap-ui" },
-    config = function()
-      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-      require("dap-python").setup(path)
-    end,
-  },
-  { "theHamsta/nvim-dap-virtual-text" },
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap-python",
+		ft = "python",
+		dependencies = { "mfussenegger/nvim-dap", "rcarriga/nvim-dap-ui" },
+		config = function()
+			local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+			require("dap-python").setup(path)
+		end,
+	},
+	{
+		"theHamsta/nvim-dap-virtual-text",
+		config = function()
+			require("nvim-dap-virtual-text").setup()
+		end,
+	},
 }
